7542313b0a039ec5c56f517972b1a8487f830f90
MakarandPundlik <mmp2308@gmail.com>
2024-10-10 22:44:19 -0400

    modified task1 to 1 liner

--- task1.sh
+++ task1.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Find the PID of infinite.sh 
-pid=$(ps aux | grep '[i]nfinite.sh' | gawk '{print $2}')
-
-# Check if a process with that PID exists
-if [ -z "$pid" ]; then
-    echo "Error: No infinite.sh process found."
-else
-    # Kill the process
-    kill $pid
-    echo "Success: infinite.sh process (PID: $pid) has been killed."
-fi
+ps aux | grep [i]nfinite.sh | grep -v grep | awk '{print $2}' | xargs kill
